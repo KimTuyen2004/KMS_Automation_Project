13f2c08200ce89795541204f3c9e827632c3dde7
tuyenkim-2004 <kimtuyen10072004@gmail.com>
2026-01-20 16:27:42 +0700

    init Project

---
+++ Object Repository/SampleTestObject.rs
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>SampleTestObject</name>
+   <tag></tag>
+   <elementGuidId>f15baa69-d1d8-4da1-bc75-dfde534b294c</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Test Suites/SampleTestSuit.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SampleTestSuit</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e368ce54-66a5-4e23-8b34-1bca8300ca00</testSuiteGuid>
+</TestSuiteEntity>
